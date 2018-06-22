f23e82026a908ccaaef722f268d1886b5e011f95
EmreOzgu <s.emre_ozgu@hotmail.com>
2018-06-22 12:16:39 -0400

    Downloads xml files too

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def find_wav(urls):
     for url in urls:
         if url.find('.wav') != -1:
             return url
+    return None
+
+def find_xml(soup):
+    for link in soup.find_all('a'):
+        if link.get('href').encode('utf-8')[-4:] == ".xml".encode('utf-8'):
+            return link.get('href')
+    return None
 
 #Download the wav file from pangloss, given the url
 def download_wav(url, lang, rec_num):
@@ -22,7 +29,17 @@ def download_wav(url, lang, rec_num):
 
     wav_url = find_wav(urls)
 
+    xml_url = find_xml(soup)
+
+    print("Downloading " + lang + "Recording" + str(rec_num + 1) + "...")
+    sys.stdout.flush()
+
+    if xml_url is not None:
+        urllib.request.urlretrieve(xml_url, lang + "Recording" + str(rec_num + 1) + ".xml")
+
     urllib.request.urlretrieve(wav_url, lang + "Recording" + str(rec_num + 1) + ".wav")
+
+    print(lang + "Recording" + str(rec_num + 1) + " download complete.") 
 
 
 #START OF SCRIPT
@@ -46,9 +63,6 @@ if not found:
     print("Language not found.")
     sys.exit(1)
 
-print("Downloading...")
-sys.stdout.flush()
-
 soup = BeautifulSoup(site, 'html.parser')
 
 urls = []
@@ -62,4 +76,4 @@ for link in soup.find_all('a'):
 for u in urls:
     download_wav(u, lang, urls.index(u))
 
-print("Download is complete.")
+print("All downloads are complete.")
