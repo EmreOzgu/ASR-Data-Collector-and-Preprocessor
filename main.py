de8698d8c8e5f248e59ea71365841e11d2f784ca
EmreOzgu <s.emre_ozgu@hotmail.com>
2018-06-23 18:54:27 -0400

    cleaned up code

--- main.py
+++ main.py
@@ -14,20 +14,19 @@ def find_wav(urls):
 
 def find_xml(soup):
     for link in soup.find_all('a'):
-        if link.get('href').encode('utf-8')[-4:] == ".xml".encode('utf-8'):
+        if link.get('href')[-4:] == ".xml":
             return link.get('href')
     return None
 
 def find_mp4(soup):
     for link in soup.find_all('a'):
-        if link.get('href').encode('utf-8')[-4:] == ".mp4".encode('utf-8'):
+        if link.get('href')[-4:] == ".mp4":
             return link.get('href')
     return None
 
 #Download the recording files from pangloss, given the url
 def download_rec(url, lang, rec_num):
     url = "http://lacito.vjf.cnrs.fr/pangloss/corpus/" + url
-    #site = urllib.request.urlopen(url)
     site = requests.get(url).content
     soup = BeautifulSoup(site, 'html.parser')
 
@@ -42,22 +41,26 @@ def download_rec(url, lang, rec_num):
 
     rec = "Recording" + str(rec_num + 1)
 
-    sys.stdout.buffer.write("Downloading ".encode('utf-8') + lang + rec.encode('utf-8') + "...".encode('utf-8'))
+    downloading = "Downloading " + lang + "Recording" + str(rec_num + 1) + "..."
+
+    #Using stdout instead of print, since print has problems printing out diacritics.
+    sys.stdout.buffer.write(downloading.encode('utf-8'))
     print()
     sys.stdout.flush()
 
     if xml_url is not None:
-        urllib.request.urlretrieve(xml_url, lang.decode('utf-8') + "Recording" + str(rec_num + 1) + ".xml")
+        urllib.request.urlretrieve(xml_url, lang + "Recording" + str(rec_num + 1) + ".xml")
     else:
         mp4_url = find_mp4(soup)
         if mp4_url is not None:
-            urllib.request.urlretrieve(mp4_url, lang.decode('utf-8') + "Recording" + str(rec_num + 1) + ".mp4")
+            urllib.request.urlretrieve(mp4_url, lang + "Recording" + str(rec_num + 1) + ".mp4")
 
     if wav_url is not None:
-        urllib.request.urlretrieve(wav_url, lang.decode('utf-8') + "Recording" + str(rec_num + 1) + ".wav")
+        urllib.request.urlretrieve(wav_url, lang + "Recording" + str(rec_num + 1) + ".wav")
 
-    #print(lang.decode('utf-8') + "Recording" + str(rec_num + 1) + " download complete.") 
-    sys.stdout.buffer.write(lang + rec.encode('utf-8') + " download complete.".encode('utf-8'))
+    complete = lang + "Recording" + str(rec_num + 1) + " download complete."
+
+    sys.stdout.buffer.write(complete.encode('utf-8'))
     print()
     sys.stdout.flush()
 
@@ -70,25 +73,22 @@ def download_all_rec(lang, site):
 
     #Find the URLs of all recordings.
     for link in soup.find_all('a'):
-        if link.get('href') is not None and link.get('href').encode('utf-8').find('show_text'.encode('utf-8')) == 0:
+        if link.get('href') is not None and link.get('href').find('show_text') == 0:
             url = link.get('href')
             urls.append(url)
 
     for u in urls:
         download_rec(u, lang, urls.index(u))
 
-    print("All downloads are complete.")
-
+#Download the recording for a given language.
 def download_lang(lang):
-
-    #find = urllib.request.urlopen("http://lacito.vjf.cnrs.fr/pangloss/corpus/index_en.html")
 
     find = requests.get("http://lacito.vjf.cnrs.fr/pangloss/corpus/index_en.html").content
 
     soup_find = BeautifulSoup(find, 'html.parser')
 
     found = False
-    
+
     #Finds the given language's website.
     for name in soup_find.find_all('a'):
         lang_name = name.text
@@ -96,8 +96,8 @@ def download_lang(lang):
         #Gets rid of parantheses for language names that include a description.
         if lang_name.find(" (") != -1:
             lang_name = lang_name[:lang_name.find(" (")]
-            
-        if lang_name.encode('utf-8') == lang:
+
+        if lang_name == lang:
             site = requests.get('http://lacito.vjf.cnrs.fr/pangloss/corpus/' + name.get('href')).content
             found = True
             break
@@ -108,9 +108,8 @@ def download_lang(lang):
 
     download_all_rec(lang, site)
 
-
+#Download recordings for all of the languages available.
 def download_all_lang():
-    #lang_list = urllib.request.urlopen("http://lacito.vjf.cnrs.fr/pangloss/corpus/index_en.html")
 
     lang_list = requests.get("http://lacito.vjf.cnrs.fr/pangloss/corpus/index_en.html").content
 
@@ -118,12 +117,11 @@ def download_all_lang():
 
     #Find all languages and download recordings.
     for link in soup_langs.find_all('a'):
-        if link.get('href') is not None and link.get('href').encode('utf-8').find('list_rsc'.encode('utf-8')) == 0:
-            lang = link.text.encode('utf-8')
-            if lang[-1:] == "n".encode('utf-8'):
-                site = requests.get("http://lacito.vjf.cnrs.fr/pangloss/corpus/" + link.get('href')).content
-                download_all_rec(lang, site)
-    
+        if link.get('href') is not None and link.get('href').find('list_rsc') == 0:
+            lang = link.text
+            site = requests.get("http://lacito.vjf.cnrs.fr/pangloss/corpus/" + link.get('href')).content
+            download_all_rec(lang, site)
+
 
 #START OF SCRIPT
 
@@ -137,4 +135,6 @@ if lang.lower() == "all":
     download_all_lang()
 else:
     lang = " ".join(args.language)
-    download_lang(lang.encode('utf-8'))
+    download_lang(lang)
+
+print("All downloads are complete.")
