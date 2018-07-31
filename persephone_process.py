f173a43aa3197df03bac791b4e7ad850b327028a
EmreOzgu <s.emre_ozgu@hotmail.com>
2018-07-31 18:05:09 -0400

    added extract_wav

--- persephone_process.py
+++ persephone_process.py
@@ -5,7 +5,7 @@ from analyze import calc_time
 from process import clean_up, find_nth_occ
 import logging
 from persephone.utterance import Utterance
-#from persephone.preprocess.wav import extract_wavs
+from persephone.preprocess.wav import extract_wavs
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s %(name)s:%(message)s', level=logging.INFO)
@@ -18,12 +18,12 @@ def divide_phonemes(file, src, txt_dest, wav_dest):
 
     alpha = file[file.rfind('_')+1:file.find('.')]
     idx = file.find('_Processed')
-    wav_name = f'{file[:idx]}.wav'
-    xml_name = f'{file[:idx]}.xml'
-    wav_path = Path(f'./Recordings_wav/{wav_name}')
-    xml_path = Path(f'./Recordings_xml/{xml_name}')
+    wav_name = file[:idx] + '.wav'
+    xml_name = file[:idx] + '.xml'
+    wav_path = Path('./Recordings_wav/' + wav_name)
+    xml_path = Path('./Recordings_xml/'+ xml_name)
 
-
+    utterances = []
         #new_file = f'{file[:idx]}_Phonemes{file[idx:]}'
 
     '''
@@ -37,23 +37,24 @@ def divide_phonemes(file, src, txt_dest, wav_dest):
             writef.write('\n')
     '''
 
-    with open(f'{src}{file}', 'r', encoding='utf-8') as readf:
+    with open(src+file, 'r', encoding='utf-8') as readf:
         for line in readf:
             id_name = line[:line.find(' ')]
-            name = f'{id_name}_{alpha}'
+            name = id_name +'_'+alpha
             try:
-                start = float(line[line.find('start=')+6:find_nth_occ(line, ' ', 2)])
-                end = round(float(line[line.find('end=')+4:find_nth_occ(line, ' ', 3)]))
+                start = float(line[line.find('start=')+6:find_nth_occ(line, ' ', 2)])*1000
+                end = float(line[line.find('end=')+4:find_nth_occ(line, ' ', 3)])*1000
             except ValueError:
                 continue
             trans = line[find_nth_occ(line, ' ', 3)+1:]
             #Put whitespace between every char.
             trans = "".join(trans.split())
             trans = " ".join(trans)
-
+            
             utter = Utterance(wav_path, xml_path, name, start, end, trans, "")
-
-            with open(f'{txt_dest}{utter.prefix}.txt', 'w', encoding='utf-8') as writef:
+            utterances.append(utter)
+            
+            with open(txt_dest+utter.prefix+'.txt', 'w', encoding='utf-8') as writef:
                 writef.write(utter.text + '\n')
             '''
             writef.write(name)
@@ -61,11 +62,13 @@ def divide_phonemes(file, src, txt_dest, wav_dest):
                 writef.write(f' {char}')
             writef.write('\n')
             '''
+
+    extract_wavs(utterances, Path('./'+wav_dest), False)
         
 if __name__ == "__main__":
     src = "Processed/"
-    txt_dest = "Utterance_xml/"
-    wav_dest = "Utterance_wav/"
+    txt_dest = "label/"
+    wav_dest = "wav/"
     skip = ["ortho", "west_uvean", "wallisian", "tiri", "bwatoo", "cemuhi", "numee", "laz", "paici", "maore_comorian", "wayana", "ngazidja_comorian", "araki", "wetamut", "yucuna", "ajie", \
             "xaracuu", "xaragure", "dehu", "nelemwa", "nemi"]
 
@@ -83,11 +86,11 @@ if __name__ == "__main__":
             divide_phonemes(file, src, txt_dest, wav_dest)
 
             idx = file.find('_Processed')
-            tree = ElementTree.parse(f'Recordings_xml/{file[:idx]}.xml')
+            tree = ElementTree.parse('Recordings_xml/'+file[:idx]+'.xml')
             root = clean_up(tree.getroot())
             time += calc_time(root)
     '''
     with open(f'{dest}total_audio.txt', 'w') as outf:
         outf.write(f'Total audio in minutes: {time/60} mins')
     '''
-    logger.info(f'Phoneme files created. Total audio in minutes: {time/60} mins.')
+    logger.info('Phoneme files created. Total audio in minutes:' + str(time/60) +' mins.')
